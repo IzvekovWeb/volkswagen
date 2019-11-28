c2cc57187775cad86357b4470f61f67aaa87fb6f
IzvekovWeb <izvekov.web@yandex.ru>
2019-11-29 01:35:31 +0300

    Созданы элементы: кнопки, header, top_slider. Сделаны иконки для стрелок и favicon. Настроена сборка под проект.

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,41 @@
-import "%modules%/header/header";
-import "%modules%/footer/footer"
+import "./slick.min.js";
+
+import jquery from 'jquery';
+window.$ = window.jQuery=jquery;
+
+$('.top_slides').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
