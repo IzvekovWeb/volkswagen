be100b0660b08911df45b7f425a3c54add447bab
IzvekovWeb <izvekov.web@yandex.ru>
2019-12-04 21:01:53 +0300

    Доделан слайдер АКЦИИ. Сделан блок НОВОСТИ. Начат блок ОСОБЕННОСТИ.

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -3,6 +3,7 @@ import "./slick.min.js";
 import jquery from 'jquery';
 window.$ = window.jQuery=jquery;
 
+
 $('.top_slides').slick({
   dots: false,
   infinite: true,
@@ -11,8 +12,8 @@ $('.top_slides').slick({
   slidesToScroll: 1,
   arrows: true,
   autoplay: true,
-  prevArrow: $('.arrow-prev'),
-  nextArrow: $('.arrow-next'),
+  prevArrow: $('.arp_1'),
+  nextArrow: $('.arn_1'),
   responsive: [
     {
       breakpoint: 1024,
@@ -37,8 +38,85 @@ $('.top_slides').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
+  ]
+});
+
+
+$('.models__slider').slick({
+  dots: true,
+  dotsClass: "my-dots",
+  infinite: true,
+  speed: 600,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: true,
+  autoplay: true,
+  prevArrow: $('.arp_2'),
+  nextArrow: $('.arn_2'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+
+
+$('.sales__slider-wrapper').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  // variableWidth: true,
+  slidesToScroll: 1,
+  arrows: true,
+  centerMode: true,
+  autoplay: false,
+  prevArrow: $('.arp_3'),
+  nextArrow: $('.arn_3'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
   ]
 });
