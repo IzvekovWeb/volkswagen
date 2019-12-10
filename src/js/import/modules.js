268568b601b0b9052e8f938ea5732eb0b661c9f3
IzvekovWeb <izvekov.web@yandex.ru>
2019-12-10 20:30:56 +0300

    Сдалан секция с формой. CSS для checkbox.

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -120,3 +120,41 @@ $('.sales__slider-wrapper').slick({
     }
   ]
 });
+
+
+
+$('.official-slider').slick({
+  dots: true,
+  dotsClass: "my-dots",
+  infinite: true,
+  speed: 600,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
